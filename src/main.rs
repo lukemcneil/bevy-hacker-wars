65399a405877b525ae949124adabf5e500d3c239
Luke McNeil <lukemcneil61@gmail.com>
2023-10-06 20:44:54 -0500

    basic with world inspector

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use bevy::{
+    input::common_conditions::input_toggle_active, prelude::*, render::camera::ScalingMode,
+};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Hacker Wars".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: true,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .add_plugins(
+            WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update, hello_world)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 256.0,
+        min_height: 144.0,
+    };
+
+    commands.spawn(camera);
+
+    let texture: Handle<Image> = asset_server.load("character.png");
+
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            ..default()
+        },
+        Name::new("Player"),
+    ));
+}
+
+fn hello_world() {
+    // println!("hello world")
+}
